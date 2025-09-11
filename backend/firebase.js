1718695659b48795c2cc62f40e56098c06aafb59
boobachad <83977152+boobachad@users.noreply.github.com>
2025-09-11 12:36:46 +0530

    2025-09-11 12:36:46 fixed the undefiend erorr int he parse logic

--- backend/firebase.js
+++ backend/firebase.js
@@ -3,7 +3,11 @@ const admin = require("firebase-admin");
 
 // Initialize Firebase Admin SDK
 try {
-  let serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  const serviceAccountString = process.env.FIREBASE_SERVICE_ACCOUNT;
+
+  if (serviceAccountString) {
+    const serviceAccountString = process.env.FIREBASE_SERVICE_ACCOUNT;
+    const serviceAccount = JSON.parse(serviceAccountString);
     if (serviceAccount.project_id) {
       if (!admin.apps.length) {
         admin.initializeApp({
@@ -13,6 +17,10 @@ try {
     } else {
       console.log("Firebase Admin SDK not initialized. 'project_id' is missing from FIREBASE_SERVICE_ACCOUNT.");
     }
+  }
+  else {
+    console.log("Firebase Admin SDK not initialized. FIREBASE_SERVICE_ACCOUNT is not set.");
+  }
 } catch (error) {
   console.error("Error initializing Firebase Admin SDK:", error.message);
 }
